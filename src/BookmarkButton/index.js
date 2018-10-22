91b99d09ce41a14daff3af804984f386ec030d05
Pedro Hernandez <pherned@gmail.com>
2018-10-22 09:34:25 -0400

    fixed radix warning

--- src/BookmarkButton/index.js
+++ src/BookmarkButton/index.js
@@ -14,7 +14,7 @@ class BookmarkButton extends Component {
     
         return (
             <div className="map-button">
-                {(this.props.bookmarks === null || (!this.props.bookmarks.includes(parseInt(this.props.apiId)))) ?
+                {(this.props.bookmarks === null || (!this.props.bookmarks.includes(parseInt(this.props.apiId), 10))) ?
                     <button className="bookmark-button" onClick={this.bookmarkIncident}>Bookmark</button>
                     : <button className="remove-button" onClick={this.removeBookmark}>Remove</button>
                 }
