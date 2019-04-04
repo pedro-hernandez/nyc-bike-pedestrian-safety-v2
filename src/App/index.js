d936f7009e00eee6ef4a7994a52acf267a724533
Pedro Hernandez <pherned@gmail.com>
2019-04-04 09:33:48 -0400

    CSS - lighter app  background image

--- src/App/index.js
+++ src/App/index.js
@@ -64,8 +64,6 @@ class App extends Component {
   }
 
   render() {
-    // console.log(this.state.recentIncidents);
-    // console.log(this.state.selectedZip);
     return <div>
       < Router >
         <div>
