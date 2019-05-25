3893be2a9ca547b696413b88ade8056326e57a91
Pedro Hernandez <pherned@gmail.com>
2019-05-25 18:04:21 -0400

    shallow rendering support added

--- src/_tests_/App_smoke.test.js
+++ src/_tests_/App_smoke.test.js
@@ -5,8 +5,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
 import { isMainThread } from 'worker_threads';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', ()=>{
+it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+});
+
+// shallow rendering example
+
+it('renders without crashing', () => {
+    shallow(<App />);
 });
